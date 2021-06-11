bf5c2246d8b931a17280060bc4bd02b7d4d9a3f6
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-06-11 16:18:16 +0200

    tdd vat number

--- src/main/java/pl/sda/tdd/VatNumber.java
+++ src/main/java/pl/sda/tdd/VatNumber.java
@@ -1,11 +1,22 @@
 package pl.sda.tdd;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.Objects;
+
 final class VatNumber {
 
-    private String vatNumber;
+    private final String vatNumber;
 
     private VatNumber(String vatNumber) {
-        this.vatNumber = vatNumber;
+        this.vatNumber = requireNonNull(vatNumber, "Vat number is null");
+        verifyVat();
+    }
+
+    private void verifyVat() {
+        if (!this.vatNumber.matches("[A-Z]{2}\\d{10}")) {
+            throw new IllegalArgumentException("Invalid vat number: " + this.vatNumber);
+        }
     }
 
     public static VatNumber from(String vatNumber) {
@@ -14,5 +25,22 @@ final class VatNumber {
 
     public String getValue() {
         return vatNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        VatNumber vatNumber1 = (VatNumber) o;
+        return vatNumber.equals(vatNumber1.vatNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(vatNumber);
     }
 }
--- src/test/java/pl/sda/tdd/VatNumberTest.java
+++ src/test/java/pl/sda/tdd/VatNumberTest.java
@@ -1,7 +1,9 @@
 package pl.sda.tdd;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,5 +16,31 @@ final class VatNumberTest {
         final var vatNumber = VatNumber.from(vat);
         // then - assertions
         assertThat(vatNumber.getValue()).isEqualTo(vat);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0302203033", "P2030303033", "PL293939930", "we9303202022"})
+    void shouldNotCreateInvalidVatNumber(String vat) {
+        assertThatCode(() -> VatNumber.from(vat))
+            .hasMessage("Invalid vat number: " + vat);
+    }
+
+    @Test
+    void shouldNotCreateNullVatNumber() {
+        assertThatCode(() -> VatNumber.from(null))
+            .hasMessage("Vat number is null");
+    }
+
+    @Test
+    void shouldCompareVatNumbers() {
+        // given
+        final var firstVat = VatNumber.from("PL1029393091");
+        final var secondVat = VatNumber.from("PL1029393092");
+        final var thirdVat = VatNumber.from("PL1029393091");
+
+        // then
+        assertThat(firstVat).isNotEqualTo(secondVat);
+        assertThat(firstVat).isEqualTo(thirdVat);
+        assertThat(secondVat).isNotEqualTo(thirdVat);
     }
 }
